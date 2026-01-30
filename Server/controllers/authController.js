3631382b51b27d1caf6d0015444343ba27fb180c
riyagupta-r <us581318@gmail.com>
2026-01-30 22:56:01 +0530

    authController updated

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -7,12 +7,12 @@ import {generateResetPasswordToken} from "../utils/generateResetPasswordToken.js
 import { generateEmailTemplate } from "../utils/generateForgotPasswordEmailTemplate.js"
 import { sendEmail } from "../utils/sendEmail.js";
 import crypto from "crypto";
-
+import {v2 as clodinary} from "cloudinary"
 
 export const register = catchAsyncErrors(async(req,res,next)=>{
        
     const {name, email, password} = req.body;
-       console.log(email)
+       
         if(!name || !email || !password){
         return next(new ErrorHandler("please provide all required fields",400));
        }
@@ -144,3 +144,156 @@ export const forgotPassword = catchAsyncErrors(async(req,res,next)=>{
     }
 });
 
+export const resetPassword = catchAsyncErrors(async(req,res,next) => {
+ 
+  //while forgotPassword we send a token through email to user 
+ 
+  const {token} = req.params;
+  
+  //hash the token
+  const resetPasswordToken = crypto.createHash("sha256").update(token).digest("hex");
+  
+  const user = await database.query(
+    "SELECT * FROM users WHERE reset_password_token = $1 AND reset_password_expire > NOW()",[resetPasswordToken]
+  ) //>now() means token expire nai hua hai abhi tk
+  
+  if(user.rows.length === 0){
+    return next (new ErrorHandler("Invalid or expired token",400))
+  }
+
+  if(req.body.password !== req.body.confirmPassword){
+    return next(new ErrorHandler("Password does not match",400));
+  }
+
+  if(
+    //password requested from frontend 
+    req.body.password?.length < 8 ||
+    req.body.password?.length >16 ||
+    req.body.confirmPassword?.length < 8 || 
+    req.body.confirmPassword?.length > 16
+  ){
+    return next(
+      new ErrorHandler ("Password must be between 8 and 20 characters.",400)
+    )
+  }
+  //hash the newPassword
+  const hashedPassword = await bcrypt.hash(req.body.password,10);
+  
+  //update in db for newPassword
+  const updateInfo = await database.query(
+    `UPDATE users SET password = $1, reset_password_token = NULL, reset_password_expire = NULL WHERE id = $2 RETURNING *`,[hashedPassword,user.rows[0].id]
+  ); //get the id for the user whom is getting updated 
+
+  //and new token is created
+  sendToken(updateInfo.rows[0],200,"Password reset successfully", res)
+
+})
+
+//only authenticated user will update this
+export const updatePassword = catchAsyncErrors ( async (req,res,next)=>{
+ const { currentPassword , newPassword , confirmNewPassword} = req.body;
+
+ if(!currentPassword || !newPassword || !confirmNewPassword){
+  return next ( new ErrorHandler("All fields are required to update password" , 400));
+ }
+ 
+ //compare user password with password we are entering currently
+ //req.user contains password 
+ const isPasswordMatch = await bcrypt.compare(currentPassword,req.user.password);
+ 
+ if(!isPasswordMatch){
+  return next(new ErrorHandler("Password is not matching with original password",401));
+  //401 forbidden
+ }
+
+ if(newPassword !== confirmNewPassword){
+  return next(new ErrorHandler("New password doesn't match with confirm new password"),400);
+ }
+
+ if(
+    //password yha pr mil  gya hai and upar handle v kra hai aagr nai mila toh error return krna hai but upar nai mila hai so ? this operator is used
+    newPassword.length < 8 ||
+    newPassword.length >16 ||
+    confirmNewPassword.length < 8 || 
+    confirmNewPassword.length > 16
+  ){
+    return next(
+      new ErrorHandler ("Password must be between 8 and 20 characters.",400)
+    )
+  }
+  //hash new password as well
+  const hashedPassword = await bcrypt.hash(newPassword,10);
+  //update new password in database
+  await database.query("UPDATE users SET password = $1 WHERE id = $2",[hashedPassword,req.user.id]);
+
+  res.status(200).json({
+    success:true,
+    message:"Password is updated successfully"
+  })
+});
+
+//only authenticated user will update profile
+export const updateProfile = catchAsyncErrors(async (req,res,next)=>{
+  
+  //name and email which is going to be updated
+  const {name , email} = req.body;
+ 
+  if(!name || !email) {
+    return next(new ErrorHandler ("Please provide all the fields..",400));
+  }
+  
+  if(name.trim().length === 0 || email.trim().length === 0 ){
+    return next ( new ErrorHandler("Name and email can not be empty",400));
+  }
+  let avatarData = {}; //var
+  
+  //req ki file m se and req ke file ke avatar hai toh avatar niklo bhar from re
+  if(req.files && req.files.avatar) {
+
+    const {avatar} = req.files; //from postman
+
+    // means??
+    if(req.user?.avatar?.public_id){
+      await cloudinary.uploader.destroy(req.user.avatar.public_id)
+    }
+   
+    const newProfileImage = await cloudinary.uploader.upload(
+      avatar.tempFilePath,{
+      folder: "Ecommerce_Avtars", //folder name where avatar will be uploaded
+      width: 150, //width
+      crop: "scale" //optimal quality
+    })
+
+    avatarData = {
+      public_id: newProfileImage.public_id,
+      url: newProfileImage.secure_url,
+    }
+    //public_id && secure_url will get from cloudinary
+  }
+  
+  let user; //variable
+
+  //if avtar data nai aya toh name and email ko update
+  if(Object.keys(avatarData).length === 0){
+
+    user = await database.query(
+      "UPDATE users SET name = $1 , email = $2 WHERE id = $3 RETURNING * ",
+      [name, email, req.user.id]
+    );
+    
+  } 
+  else{
+    user = await database.query
+      ("UPDATE users SET name = $1 , email = $2, avatar = $3 WHERE id= $4 RETURNING *",
+      [name, email, avatarData, req.user.id]
+    )
+  }
+  console.log("1111111111111")
+ 
+  //ressponse
+  res.status(200).json({
+    success: true,
+    message: "Profile updated Sucessfully",
+    user: user.rows[0]
+  })
+})
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -3,7 +3,10 @@ import {register
     ,login
     ,logout
     ,getUser
-    ,forgotPassword} from "../controllers/authController.js";
+    ,forgotPassword,
+    resetPassword,
+    updatePassword,
+    updateProfile} from "../controllers/authController.js";
 import {isAuthenticated} from "../middlewares/authMiddleware.js"
     const router = express.Router();
 
@@ -13,5 +16,7 @@ router.post("/login",login);
 router.get("/me",isAuthenticated,getUser);
 router.get("/logout",isAuthenticated,logout);
 router.post("/password/forgot",forgotPassword);
-
+router.put("/password/reset/:token" , resetPassword)
+router.put("/password/update",isAuthenticated,updatePassword)
+router.put("/profile/update" ,  isAuthenticated , updateProfile)
 export default router;
