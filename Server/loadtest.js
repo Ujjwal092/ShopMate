f5fa819dbb96a6d713ea61058148251d06194d11
Ujjwal092 <us581318@gmail.com>
2026-06-15 23:48:26 +0530

    commit

--- Server/loadtest.js
+++ Server/loadtest.js
@@ -11,19 +11,19 @@
 //   console.log(`Status: ${res.status}`);
 //   sleep(1);
 // }
-import http from "k6/http";
-import { sleep } from "k6";
+// import http from "k6/http";
+// import { sleep } from "k6";
 
-export let options = {
-  stages: [
-    { duration: "30s", target: 100 },
-    { duration: "1m", target: 500 },
-    { duration: "30s", target: 1000 },
-    { duration: "30s", target: 0 },
-  ],
-};
+// export let options = {
+//   stages: [
+//     { duration: "30s", target: 100 },
+//     { duration: "1m", target: 500 },
+//     { duration: "30s", target: 1000 },
+//     { duration: "30s", target: 0 },
+//   ],
+// };
 
-export default function () {
-  http.get("http://localhost:4000/api/v1/product");
-  sleep(1);
-}
+// export default function () {
+//   http.get("http://localhost:4000/api/v1/product");
+//   sleep(1);
+// }
