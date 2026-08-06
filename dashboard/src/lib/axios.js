7380ddfd8e53abede95fa3813e7a98d3260f6496
Ujjwal092 <us581318@gmail.com>
2026-08-06 19:34:15 +0530

    axios configured

--- dashboard/src/lib/axios.js
+++ dashboard/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://cartsyy.onrender.com/api/v1",
   withCredentials: true,
 });
