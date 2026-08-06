a3b7c9fe370f6b0c8308b465eabf177dfeb1f331
Ujjwal092 <us581318@gmail.com>
2026-08-06 19:26:59 +0530

    frontend deployment

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 //axios is used to make HTTP requests to the backend server
 export const axiosInstance = axios.create({
   baseURL:
-  import.meta.env.MODE === "development" 
-  ? "http://localhost:4000/api/v1"
-  :"/",
+    import.meta.env.MODE === "development"
+      ? "https://cartsyy.onrender.com/api/v1"
+      : "/",
   withCredentials: true,
 });
 //console.log("Axios base URL:", axiosInstance.defaults.baseURL);
